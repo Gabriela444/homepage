a4e1f95aab4945082c63eca6382097e36e9ae95f
Gabriela Małecka <malecka.gabriela02@gmail.com>
2023-05-15 22:24:30 +0200

    specifying functions in code

--- js/script.js
+++ js/script.js
@@ -1,32 +1,49 @@
-
-
-let js_buttonSoap = document.querySelector(".js_buttonSoap");
-let js_photoSoap = document.querySelector(".js_photoSoap");
-
-let js_buttonIncense = document.querySelector(".js_buttonIncense");
-let js_photoIncense = document.querySelector(".js_photoIncense");
-
-let js_buttonTheme = document.querySelector(".js_buttonTheme");
-let body = document.querySelector(".body");
-let button__themeName = document.querySelector(".button__themeName");
-
-js_buttonSoap.addEventListener("click", () => {
-    js_photoSoap.classList.toggle("photo__hide");
-    js_buttonSoap.innerText = js_photoSoap.classList.contains("photo__hide") ?
-        "Pokaż zdjęcie" : "Ukryj zdjęcie";
-});
-
-js_buttonIncense.addEventListener("click", () => {
-    js_photoIncense.classList.toggle("photo__hide");
-    js_buttonIncense.innerText = js_photoIncense.classList.contains("photo__hide") ?
-        "Pokaż zdjęcie" : "Ukryj zdjęcie";
-});
-
-js_buttonTheme.addEventListener("click", () => {
-    body.classList.toggle("button__themeBlue");
-    if (body.classList.contains("button__themeBlue")) {
-        button__themeName.innerText = "zielony";
-    } else {
-        button__themeName.innerText = "niebieski";
+{
+    const welcome = () => {
+        console.log("Hello! Have a nice day. :)")
     }
-});
+
+    const hideShowSoapPhoto = () => {
+        const js_photoSoap = document.querySelector(".js_photoSoap");
+
+        js_photoSoap.classList.toggle("photo__hide");
+        js_buttonSoap.innerText = js_photoSoap.classList.contains("photo__hide") ?
+            "Pokaż zdjęcie" : "Ukryj zdjęcie";
+
+    }
+
+
+    const hideShowIncensePhoto = () => {
+        const js_photoIncense = document.querySelector(".js_photoIncense");
+
+
+        js_photoIncense.classList.toggle("photo__hide");
+        js_buttonIncense.innerText = js_photoIncense.classList.contains("photo__hide") ?
+            "Pokaż zdjęcie" : "Ukryj zdjęcie";
+
+    }
+
+    const toggleBackground = () => {
+        const body = document.querySelector(".body");
+        const button__themeName = document.querySelector(".button__themeName");
+
+        body.classList.toggle("button__themeBlue");
+        button__themeName.innerText = body.classList.contains("button__themeBlue") ? "zielony" : "niebieski";
+    }
+
+
+    const init = () => {
+        const js_buttonSoap = document.querySelector(".js_buttonSoap");
+        js_buttonSoap.addEventListener("click", hideShowSoapPhoto);
+
+        const js_buttonIncense = document.querySelector(".js_buttonIncense");
+        js_buttonIncense.addEventListener("click", hideShowIncensePhoto);
+
+        const js_buttonTheme = document.querySelector(".js_buttonTheme");
+        js_buttonTheme.addEventListener("click", toggleBackground);
+
+        welcome();
+    }
+
+    init();
+}
